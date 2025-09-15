d9898be43ac72f86ea38baa7c7d21af4132f2825
thibo24 <thibault.ripoche@free.fr>
2025-09-15 12:13:26 +0200

    add bff project

---
+++ bff/BffGreenWaiter/src/main/java/fr/green/BffGreenWaiter/BffGreenWaiterApplication.java
@@ -0,0 +1,13 @@
+package fr.green.BffGreenWaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffGreenWaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffGreenWaiterApplication.class, args);
+	}
+
+}
