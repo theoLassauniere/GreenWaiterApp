9b88984a1a6c62fd21fd23f4af235a730fe0c1e7
Julie <julie.seyier@etu.univ-cotedazur.fr>
2025-09-20 16:23:21 +0200

    :sparkles: Add of tables, tables-filter and occupied-tables-checkbox components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,12 @@
 import "./App.css";
-import Table from "./components/table/table";
+import Tables from "./components/tables/tables";
+import { mockTables } from "./mocks/tables";
 
 function App() {
     return (
         <div className="app">
             <h1>Nom du restaurant</h1>
-            <div className="tables">
-                <Table
-                    id={1}
-                    capacity={4}
-                    occupied={true}
-                    isCommandesPage={true}
-                    commandState="preparing-in-kitchen"
-                    commandPreparationPlace="bar"
-                />
-                <Table
-                    id={2}
-                    capacity={2}
-                    occupied={false}
-                    isCommandesPage={false}
-                />
-                <Table
-                    id={3}
-                    capacity={2}
-                    occupied={true}
-                    isCommandesPage={true}
-                    commandState="awaiting-service"
-                    commandPreparationPlace="bar"
-                />
-                <Table
-                    id={4}
-                    capacity={6}
-                    occupied={true}
-                    isCommandesPage={true}
-                    commandState="served"
-                    commandPreparationPlace="cuisine"
-                />
-            </div>
+            <Tables tables={mockTables} />
         </div>
     );
 }
