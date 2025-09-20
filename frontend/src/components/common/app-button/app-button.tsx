a9121981a33ac47f362780a808f1a2fb5c4719d5
theoLassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-09-20 21:22:32 +0200

    feat: added app button component

---
+++ frontend/src/components/common/app-button/app-button.tsx
@@ -0,0 +1,22 @@
+import "./app-button.scss";
+
+type ButtonProps = {
+    label: string;
+    onClick?: () => void;
+    className?: string;
+};
+
+export default function AppButton({
+                                      label,
+                                      onClick,
+                                      className = "",
+                                  }: ButtonProps) {
+    return (
+        <button
+            className={`custom-btn ${className}`}
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    );
+}
