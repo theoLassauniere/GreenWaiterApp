733ab2f5276a8e35b789c2737442beffbdf6c9f3
theoLassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2025-09-20 21:22:43 +0200

    feat: added sidebar container

---
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,14 @@
+import AppButton from "../common/app-button/app-button.tsx";
+import "./sidebar.scss"
+
+export default function Sidebar() {
+    return(
+        <div className="sidebar-container">
+            <AppButton label="Tables"/>
+            <AppButton label="Menu"/>
+            <AppButton label="Commandes"/>
+            <AppButton label="Paiement"/>
+        </div>
+
+    )
+};
