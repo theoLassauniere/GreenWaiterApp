09bbf2268a19c8d35dcca61a3a53ec269f4a419c
thibo24 <98901782+thibo24@users.noreply.github.com>
2025-09-21 21:01:42 +0200

    25 add of the fooditem component (#38)
    * wip
    
    * adding menu Item
    
    * adding menu item to app.tsx to demonstrate what it look like

---
+++ frontend/src/components/menu/MenuItem/MenuItem.tsx
@@ -0,0 +1,39 @@
+import type { Item } from '../../../model/Item.ts';
+import type { ReactNode } from 'react';
+import { useState } from 'react';
+import './MenuItem.css';
+import IconButton from '../../common/IconButton/IconButton.tsx';
+import AllergenPopup from '../AllergenPopup/AllergenPopup.tsx';
+
+type MenuItemProps = {
+  item: Item;
+  onClick?: () => void;
+};
+
+export default function MenuItem({ item, onClick }: MenuItemProps): ReactNode {
+  const { imageUrl, name, price, allergens } = item;
+  const [isOpen, setOpen] = useState(false);
+  const open = () => setOpen(true);
+  const close = () => setOpen(false);
+  return (
+    <>
+      <div className="menu-item" onClick={onClick}>
+        <IconButton className="icon-button-menu-item" icon="info" onClick={open} />
+        <div className="item-img-container">
+          <img className="item-img" src={imageUrl ?? ''} alt={name} />
+        </div>
+        <div className="item-text-container">
+          <div>
+            {name} - {price}€
+          </div>
+        </div>
+      </div>
+      <AllergenPopup
+        isOpen={isOpen}
+        onClose={close}
+        allergens={allergens}
+        title={`Allergènes - ${name}`}
+      />
+    </>
+  );
+}
