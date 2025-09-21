d55ccb3bfc41050df682a6830fa956b4c95d4da7
Théo Lassauniere <120557548+theoLassauniere@users.noreply.github.com>
2025-09-21 22:41:09 +0200

    Added the app sidebar (#37)
    * chore: added scss dependencies
    
    * feat: added app button component
    
    * feat: added sidebar container
    
    * feat: added sidebar to the app page
    
    # Conflicts:
    #	frontend/src/App.css
    #	frontend/src/App.tsx
    
    * refactor: updated app buttons style to fit better to the tablet format
    
    * chore: resolve rebase conflits
    
    ---------
    
    Co-authored-by: Mathis-Jullien <mathis.jullien@etu.univ-cotedazur.fr>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,55 @@
-import './App.css';
+import './App.scss';
 import Table from './components/table/table';
 import MenuItemSelection from './pages/MenuItemSelection.tsx';
 import mockData from './components/mockData.ts';
+import Sidebar from "./components/sidebar/sidebar.tsx";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Nom du restaurant</h1>
-      <div className="tables">
-        <Table
-          id={1}
-          capacity={4}
-          occupied={true}
-          isCommandesPage={true}
-          commandState="preparing-in-kitchen"
-          commandPreparationPlace="bar"
-        />
-        <Table id={2} capacity={2} occupied={false} isCommandesPage={false} />
-        <Table
-          id={3}
-          capacity={2}
-          occupied={true}
-          isCommandesPage={true}
-          commandState="awaiting-service"
-          commandPreparationPlace="bar"
-        />
-        <Table
-          id={4}
-          capacity={6}
-          occupied={true}
-          isCommandesPage={true}
-          commandState="served"
-          commandPreparationPlace="cuisine"
-        />
-      </div>
-
-      <div className="item">
-        <MenuItemSelection listItems={mockData} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="sidebar">
+                <Sidebar/>
+            </div>
+            <main>
+                <div className="tables">
+                    <h1>Nom du restaurant</h1>
+                    <Table
+                        id={1}
+                        capacity={4}
+                        occupied={true}
+                        isCommandesPage={true}
+                        commandState="preparing-in-kitchen"
+                        commandPreparationPlace="bar"
+                    />
+                    <Table
+                        id={2}
+                        capacity={2}
+                        occupied={false}
+                        isCommandesPage={false}
+                    />
+                    <Table
+                        id={3}
+                        capacity={2}
+                        occupied={true}
+                        isCommandesPage={true}
+                        commandState="awaiting-service"
+                        commandPreparationPlace="bar"
+                    />
+                    <Table
+                        id={4}
+                        capacity={6}
+                        occupied={true}
+                        isCommandesPage={true}
+                        commandState="served"
+                        commandPreparationPlace="cuisine"
+                    />
+                </div>
+                <div className="item">
+                  <MenuItemSelection listItems={mockData} />
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
