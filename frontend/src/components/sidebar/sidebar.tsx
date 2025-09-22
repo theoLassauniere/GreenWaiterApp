cfbf40a343deb01622af8387d1865315b00d1232
Mathis-Jullien <mathis.jullien@etu.univ-cotedazur.fr>
2025-09-22 11:29:20 +0200

    chore: format after rebase

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -1,17 +1,17 @@
-import AppButton from "../common/app-button/app-button.tsx";
-import "./sidebar.scss"
+import AppButton from '../common/app-button/app-button.tsx';
+import './sidebar.scss';
 
 type SidebarProps = {
-    onSelect: (page: "tables" | "menu" | "commandes" | "paiement") => void;
+  onSelect: (page: 'tables' | 'menu' | 'commandes' | 'paiement') => void;
 };
 
 export default function Sidebar({ onSelect }: SidebarProps) {
-    return (
-        <div className="sidebar-container">
-            <AppButton label="Tables" onClick={() => onSelect("tables")} />
-            <AppButton label="Menu" onClick={() => onSelect("menu")} />
-            <AppButton label="Commandes" onClick={() => onSelect("commandes")} />
-            <AppButton label="Paiement" onClick={() => onSelect("paiement")} />
-        </div>
-    );
+  return (
+    <div className="sidebar-container">
+      <AppButton label="Tables" onClick={() => onSelect('tables')} />
+      <AppButton label="Menu" onClick={() => onSelect('menu')} />
+      <AppButton label="Commandes" onClick={() => onSelect('commandes')} />
+      <AppButton label="Paiement" onClick={() => onSelect('paiement')} />
+    </div>
+  );
 }
