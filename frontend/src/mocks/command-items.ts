96bb730d06f8a579e4f53214d5514cc93ebac3f6
Mathis-Jullien <147171630+Mathis-Jullien@users.noreply.github.com>
2025-09-28 23:05:33 +0200

    Add the payment item detail component (#48)
    * feat: add the payment item detail component with mocks
    
    * fix: select all sets quantities to the maximum
    
    * fix: select all unchecked when if not every item has max quantity
    
    * feat: implement item detail component and update payment page layout
    
    * fix: delete Payment.tsx
    
    * refactor: rename IconButton files and update stylesheet import
    
    * refactor: format code
    
    * fix: review comments

---
+++ frontend/src/mocks/command-items.ts
@@ -0,0 +1,67 @@
+import type { CommandItem } from '../models/CommandItem.ts';
+
+export const mockCommandItems: CommandItem[] = [
+  {
+    id: 1,
+    name: 'Coca Cola',
+    quantity: 3,
+    categoryId: 1,
+    price: 3.0,
+  },
+  {
+    id: 2,
+    name: 'Pastaga',
+    quantity: 1,
+    categoryId: 2,
+    price: 4,
+  },
+  {
+    id: 3,
+    name: 'Pizza Margherita',
+    quantity: 2,
+    categoryId: 3,
+    price: 9.5,
+  },
+  {
+    id: 4,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+  {
+    id: 5,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+  {
+    id: 6,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+  {
+    id: 7,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+  {
+    id: 8,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+  {
+    id: 9,
+    name: 'Cheesecake',
+    quantity: 1,
+    categoryId: 4,
+    price: 5.5,
+  },
+];
