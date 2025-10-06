2d36fc8f7bd2fba9a89b29ec42cdd9b1ef1b6c7d
Mathis-Jullien <mathis.jullien@etu.univ-cotedazur.fr>
2025-10-06 11:38:29 +0200

    refactor: rename App files to lowercase to avoid case sensitivity issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-import App from './App-wrong.tsx';
+import App from './app.tsx';
 
 createRoot(document.getElementById('root')!).render(<App />);
