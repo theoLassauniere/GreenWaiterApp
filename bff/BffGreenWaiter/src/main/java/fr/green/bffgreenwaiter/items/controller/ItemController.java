7894bcab9d732fcd13a510f31252d1d2f24e19c6
thibo24 <thibault.ripoche@free.fr>
2025-11-03 09:00:30 +0100

    wip

--- bff/BffGreenWaiter/src/main/java/fr/green/bffgreenwaiter/items/controller/ItemController.java
+++ bff/BffGreenWaiter/src/main/java/fr/green/bffgreenwaiter/items/controller/ItemController.java
@@ -1,6 +1,8 @@
 package fr.green.bffgreenwaiter.items.controller;
 
+import fr.green.bffgreenwaiter.items.model.GroupMenu;
 import fr.green.bffgreenwaiter.items.model.Item;
+import fr.green.bffgreenwaiter.items.service.GroupMenuService;
 import fr.green.bffgreenwaiter.items.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+    private final GroupMenuService groupMenuService;
 
     @GetMapping("/getItems/{category}")
     public List<Item> getItemsByCategory(@PathVariable String category) {
         return itemService.getItemsByCategory(category);
+    }
+
+    @GetMapping("/getMenu")
+    public GroupMenu getMenu() {
+        return groupMenuService.getMenu();
     }
 }
